473815ca0b120c0ca98d24ad959969c7a17d9a64
matutosi <matutosi@gmail.com>
2022-05-27 09:53:08 +0900

    improve mecab

--- R/mecab.R
+++ R/mecab.R
@@ -4,15 +4,14 @@ mecabUI <- function(id) {
   tagList(
     sidebarLayout(
       sidebarPanel(
-
-
-
+        textInput(ns("bin_dir"), "Directory of MeCab bin", value = "d:/pf/mecab/bin"),
+        selectInput(ns("iconv"), "Convert encoding of MeCab output", choices = c("CP932_UTF-8", "", "EUC_UTF-8")),
       ),
 
       mainPanel(
-
-       reactableOutput(ns("table")),
-
+        shinycssloaders::withSpinner(type = sample(1:8, 1), color.background = "white",
+          reactableOutput(ns("table")),
+        ),
       ),
 
     )
@@ -23,20 +22,24 @@ mecabUI <- function(id) {
 mecabServer <- function(id, data_in){
   moduleServer(id, function(input, output, session){
 
-    # 
-    res_moranajp <- reactive({
+    # Run moranajp_all
+    mecab <- reactive({
+      bin_dir  <- input$bin_dir
+      text_col <- colnames(data_in)[1]
+      iconv    <- input$iconv
 
-  #       bin_dir  <- "d:/pf/mecab/bin/"
-  #       text_col <- "text"
-  #       iconv    <- ""
-  # 
-  #       data_in() %>%
-  #         moranajp::moranajp_all(bin_dir = bin_dir, text_col = text_col, iconv = iconv)
+      data_in %>%
+        moranajp::moranajp_all(bin_dir = bin_dir, text_col = text_col, iconv = iconv) %>%
+        dplyr::select(term = 9, pos0 = 3, pos1 = 4)
+    })
 
+    # Show table
+    output$table <- renderReactable({
+      reactable::reactable(mecab(), resizable = TRUE, filterable = TRUE, searchable = TRUE,)
     })
 
     # Return
-
+    reactive({ mecab() }) 
 
   })
 }
