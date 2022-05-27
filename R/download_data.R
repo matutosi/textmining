845119dafb42ff448fe2aef12581ed24b0d73eef
matutosi <matutosi@gmail.com>
2022-05-27 09:52:35 +0900

    rename

--- R/download_data.R
+++ R/download_data.R
@@ -1,5 +1,5 @@
 ## UI module
-data_download_tsvUI <- function(id, label = "Download file") {
+download_tsv_dataUI <- function(id, label = "Download file") {
   ns <- NS(id)
   tagList(
     downloadButton(ns("data_download_tsv"), "Download tsv data"),
@@ -7,7 +7,7 @@ data_download_tsvUI <- function(id, label = "Download file") {
 }
 
 ## Server module
-data_download_tsvSever <- function(id, data, filename){
+download_tsv_dataServer <- function(id, data, filename){
   moduleServer(id, function(input, output, session){
     output$data_download_tsv = downloadHandler(
       filename = paste0(filename, ".tsv"),
